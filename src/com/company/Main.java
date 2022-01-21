738ad7eba1409bdee5156dff0847ea454268d927
Georgii <loveOlechka@mail.ru>
2022-01-21 14:27:15 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        homework1();
+        homework2();
+        homework3();
+        homewaork4();
+    }
+
+    public static void homework1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + "" + firstName + "" + middleName;
+        System.out.print("ФИО сотрудника — " + fullName);
+
+    }
+
+    public static void homework2() {
+    }
+
+    public static void homework3() {
+    }
+
+    public static void homewaork4() {
+    }
+}
+
+
+
+
