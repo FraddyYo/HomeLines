74530b0397fe2e046d82155d20dd816196f88600
Georgii <loveOlechka@mail.ru>
2022-01-21 16:51:15 +0300

    Как 4 задачу сделать через дургие методы я так и не понял)))

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,18 +13,26 @@ public class Main {
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
-        String fullName = lastName + "" + firstName + "" + middleName;
-        System.out.print("ФИО сотрудника — " + fullName);
-
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 
     public static void homework2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        String fullName2 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName2);
     }
 
     public static void homework3() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        String newFullName = fullName.replace(' ', ';');
+        System.out.println("Данные ФИО сотрудника для административного отдела — " + newFullName);
     }
 
     public static void homewaork4() {
+        String fullName = "Иванов Семён Семёнович";
+        String newFullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + newFullName);
     }
 }
 
